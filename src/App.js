bc53d6ce708df3411c1c571857ef79ad854749d1
Mila Gvozdeva <103929930+KaSofi@users.noreply.github.com>
2023-03-17 22:03:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import FruitsClass from './component/FruitsClass';
 
 
 const fruits = [
-  {'id':' 0','picture': 'https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 'title': 'Яблоко', 'description': 'Самое полезное в яблоках – макроэлемент калий. Он оказывает комплексное действие: с одной стороны – мочегонное, с другой – благотворно действует на сердце, поддерживает нашу нервную систему.', 'price': '100P'},
+  {id: 0, picture: 'https://images.unsplash.com/photo-1570913149827-d2ac84ab3f9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', title: 'Яблоко', description: 'Самое полезное в яблоках – макроэлемент калий. Он оказывает комплексное действие: с одной стороны – мочегонное, с другой – благотворно действует на сердце, поддерживает нашу нервную систему.', price: '100P'},
 
-  {'id':' 1','picture': 'https://images.unsplash.com/photo-1620217491382-4772d58bc863?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', 'title': 'Апельсин', 'description': 'Чрезвычайно полезный фрукт, в его составе – бета-каротин, фолиевая кислота, витамины группы В, аскорбиновая кислота (витамин С), витамины Н и РР. Наши апельсины самые вкусные.', 'price': '200P'},
+  {id: 1, picture: 'https://images.unsplash.com/photo-1620217491382-4772d58bc863?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', title: 'Апельсин', description: 'Чрезвычайно полезный фрукт, в его составе – бета-каротин, фолиевая кислота, витамины группы В, аскорбиновая кислота (витамин С), витамины Н и РР. Наши апельсины самые вкусные.', price: '200P'},
 
-  {'id':' 2','picture': 'https://images.unsplash.com/photo-1528825871115-3581a5387919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80', 'title': 'Банан', 'description': 'Они богаты витаминами А, В6, С, Е и К, а также считаются хорошим источником кальция, фосфора, магния, калия и натрия. Благодаря своему составу бананы помогают, к примеру, в борьбе с анемией, менструальными болями.', 'price': '100P'},
+  {id: 2, picture: 'https://images.unsplash.com/photo-1528825871115-3581a5387919?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80', title: 'Банан', description: 'Они богаты витаминами А, В6, С, Е и К, а также считаются хорошим источником кальция, фосфора, магния, калия и натрия. Благодаря своему составу бананы помогают, к примеру, в борьбе с анемией, менструальными болями.', price: '100P'},
 
-  {'id':' 3','picture': 'https://images.unsplash.com/photo-1589820296156-2454bb8a6ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', 'title': 'Ананас', 'description': 'тропический фрукт, содержащий много ферментов, витаминов и антиоксидантов. Особенно он ценится за высокое содержание витамина С, марганца и бромелайна. Наши ананасы самые спелые.', 'price': '500P'}
+  {id: 3, picture: 'https://images.unsplash.com/photo-1589820296156-2454bb8a6ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80', title: 'Ананас', description: 'тропический фрукт, содержащий много ферментов, витаминов и антиоксидантов. Особенно он ценится за высокое содержание витамина С, марганца и бромелайна. Наши ананасы самые спелые.', price: '500P'}
 ]
 console.log(fruits)
 
